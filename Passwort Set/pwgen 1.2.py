7719898e99f143648209c436a307ab3550236e2d
Hilko <91159261+HilkoCFisi@users.noreply.github.com>
2022-05-19 12:20:41 +0200

    Add files via upload

---
+++ Passwort Set/pwgen 1.2.py
@@ -0,0 +1,23 @@
+import secrets
+groß = "QWERTZUIOPASDFGHJKLYXCVBNM"
+klein = "qwertzuiopasdfghjklyxcvbnm"
+zahl = "1234567890"
+sonder = "!§$%&=?@"
+neu = "y"
+while neu == "y":
+    pw = ""
+    length = int(input("Wie viele Zeichen soll das Passwort haben? "))
+    for _ in range(length):
+        pw = pw+secrets.choice(groß + klein + str(zahl) + sonder)
+    print(pw)
+    speichern = input("Passwort speichern? Ja = y | Nein = n | Eingabe: ")
+    if speichern == "y":
+        datname = str(input('Wie soll die Datei heißen? '))
+        datei = open(datname +'.txt' , 'w')
+        datei.write(pw)
+        break
+    elif speichern == "n":   
+        neu = input("Neues Passwort generieren? Ja = y | Nein = n | Eingabe: ")
+    else: print("Ungültige Eingabe")
+input("Drücke Enter zum schleßen.")
+
